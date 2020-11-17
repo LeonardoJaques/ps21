50955839821c13bfe97fbaed6bc099adfe7e3f6d
LeonardoJaques <jaques.projetos@outlook.com>
2020-11-16 22:13:37 -0300

    -first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Footer from "./partials/Footer";
+import Header from "./partials/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
