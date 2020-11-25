d0f91c54bbd8055ab440163a90cfcbd5be8961c0
LeonardoJaques <jaques.projetos@outlook.com>
2020-11-24 23:09:11 -0300

    change header color and css positions

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -4,32 +4,23 @@ import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-import {
-  createMuiTheme,
-  ThemeProvider,
-  makeStyles,
-} from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       // light: será calculada com base em palette.primary.main,
-      main: "#222831",
+      main: "#fbf6f0",
       // dark: será calculada com base em palette.primary.main,
       // contrastText: será calculada para contrastar com palette.primary.main
     },
     secondary: {
-      light: "#fbf6f0",
-      main: "#0044ff",
+      light: "#393e46",
+      main: "#393e46",
       // dark: será calculada com base palette.secondary.main,
-      contrastText: "#ffcc00",
+      contrastText: "#222831",
     },
-    // Usado por `getContrastText()` para maximizar o contraste entre
-    // o plano de fundo e o texto.
     contrastThreshold: 3,
-    // Usado pelas funções abaixo para mudança de uma cor de luminância por aproximadamente
-    // dois índices dentro de sua paleta tonal.
-    // Por exemplo, mude de Red 500 para Red 300 ou Red 700.
     tonalOffset: 0.2,
   },
 });
@@ -43,22 +34,21 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <span className={styles.logoText}>Leoanardo Jaques</span>
-      <nav className={styles.menu}>
-        <ThemeProvider theme={theme}>
-          <Paper square={false} elevation={3}>
+      <nav className={styles.nav}>
+        <Paper elevation={4} className={styles.paper}>
+          <ThemeProvider theme={theme}>
             <Tabs
+              className={styles.tabs}
               value={value}
-              indicatorColor="primary"
-              textColor="secondary"
               onChange={handleChange}
-              // aria-label="disabled tabs example"
+              indicatorColor="primary"
             >
               <Tab label="Blog" selected />
               <Tab label="Sobre" />
               <Tab label="Contato" />
             </Tabs>
-          </Paper>
-        </ThemeProvider>
+          </ThemeProvider>
+        </Paper>
       </nav>
     </header>
   );
